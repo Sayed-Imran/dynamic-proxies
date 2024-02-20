e99f2c71c060ac49ce1d4125266e640c01d8f921
Sayed-Imran <sayedimran00786@gmail.com>
2024-02-20 22:27:52 +0530

    Add Kubernetes deployment creation in main function

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/sayed-imran/dynamic-proxies/handlers"
+)
+
+func main() {
+	var kubeHandler = handlers.KubernetesHandler{
+		Namespace: "default",
+		Name:      "myapp",
+		Image:     "nginx:1.14.2",
+		Replicas:  3,
+		Port:      80,
+	}
+	err := kubeHandler.CreateDeployment()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
