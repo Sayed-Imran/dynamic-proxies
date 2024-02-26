d59b86999513c4dd7825964b231f0ca3cd411e54
Sayed-Imran <sayedimran00786@gmail.com>
2024-02-26 15:23:59 +0530

    Add dynamic client and create service and virtual service

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 
 func main() {
 	clientset := config.KubeClient.Clientset
+	DynamicClient := config.KubeClient.DynamicClient
 	var kubeHandler = handlers.KubernetesHandler{
 		Clientset: clientset,
+		DynamicClient: DynamicClient,
 		Namespace: config.Configuration.Namespace,
 		Name:      "myapp",
 		Image:     "nginx:1.14.2",
@@ -17,4 +19,8 @@ func main() {
 	}
 	err := kubeHandler.CreateDeployment()
 	handlers.ErrorHandler(err, "Error creating deployment")
+	err = kubeHandler.CreateService()
+	handlers.ErrorHandler(err, "Error creating service")
+	err = kubeHandler.CreateVirtualService()
+	handlers.ErrorHandler(err, "Error creating virtual service")
 }
