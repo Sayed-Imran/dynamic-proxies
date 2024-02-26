d49176d9870f825773ffcc94ab9b5e91d3a6067a
Sayed-Imran <sayedimran00786@gmail.com>
2024-02-26 12:09:25 +0530

    Refactor main function to use config package for Kubernetes client and namespace

--- main.go
+++ main.go
@@ -3,27 +3,18 @@ package main
 import (
 	"github.com/sayed-imran/dynamic-proxies/config"
 	"github.com/sayed-imran/dynamic-proxies/handlers"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-
-	baseConfig, err := config.LoadConfig()
-	handlers.ErrorHandler(err, "Error loading config")
-
-	config, err := clientcmd.BuildConfigFromFlags("", baseConfig.KubeconfigPath)
-	handlers.ErrorHandler(err, "Error building kubeconfig")
-	clientset, err := kubernetes.NewForConfig(config)
-	handlers.ErrorHandler(err, "Error building clientset")
+	clientset := config.KubeClient()
 	var kubeHandler = handlers.KubernetesHandler{
 		Clientset: clientset,
-		Namespace: baseConfig.Namespace,
+		Namespace: config.Configuration.Namespace,
 		Name:      "myapp",
 		Image:     "nginx:1.14.2",
 		Replicas:  3,
 		Port:      80,
 	}
-	err = kubeHandler.CreateDeployment()
+	err := kubeHandler.CreateDeployment()
 	handlers.ErrorHandler(err, "Error creating deployment")
 }
