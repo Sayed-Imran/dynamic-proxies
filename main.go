164ea289dcca4bd870c436bfea371e1091d1b21d
Sayed-Imran <sayedimran00786@gmail.com>
2024-03-06 22:09:47 +0530

    Refactor main function and add API endpoint for creating microservice

--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"github.com/sayed-imran/dynamic-proxies/handlers"
-	errorHandler "github.com/sayed-imran/dynamic-proxies/handlers/error_handler"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sayed-imran/dynamic-proxies/config"
+	apihandler "github.com/sayed-imran/dynamic-proxies/handlers/api_handler"
 )
 
 func main() {
-	var microservice = handlers.Microservice{
-		Name:     "app-1",
-		Image:    "sayedimran/fastapi-sample-app:v4",
-		Replicas: 2,
-		Port:     7000,
-	}
-	err := microservice.CreateMicroservice()
-	errorHandler.ErrorHandler(err, "Error creating microservice")
-	logs:= microservice.GetMicroserviceLogs()
-	println(logs)
+	// var microservice = handlers.Microservice{
+	// 	Name:     "app-1",
+	// 	Image:    "sayedimran/fastapi-sample-app:v4",
+	// 	Replicas: 2,
+	// 	Port:     7000,
+	// }
+	// err := microservice.CreateMicroservice()
+	// errorHandler.ErrorHandler(err, "Error creating microservice")
+	// logs:= microservice.GetMicroserviceLogs()
+	// println(logs)
+	router := gin.Default()
+	router.POST("/create-microservice", apihandler.CreateMicroservice)
+	router.Run(fmt.Sprintf(":%s", config.Configuration.Port))
 }
--- web/schema/schema.go
+++ web/schema/schema.go
@@ -3,11 +3,10 @@ package apischema
 type DeployConfig struct {
 	AppName  string `json:"name"`
 	Image    string `json:"image"`
-	Replicas int32    `json:"replicas"`
-	Port     int32    `json:"port"`
+	Replicas int32  `json:"replicas"`
+	Port     int32  `json:"port"`
 }
 
 type DeleteConfig struct {
 	AppName string `json:"name"`
 }
-
